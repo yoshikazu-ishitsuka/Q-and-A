e4cfd4ce7d89fecb97205a8b45f57e777667547a
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-05-16 10:57:57 +0900

    Questionsコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+  get 'questions/show'
+  get 'questions/new'
+  get 'questions/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
