f884e8c6afe165befae75921d7db9321a7d9200e
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-05-16 14:26:07 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'questions/index'
-  get 'questions/show'
-  get 'questions/new'
-  get 'questions/edit'
+  # get 'questions/index'
+  # get 'questions/show'
+  # get 'questions/new'
+  # get 'questions/edit'
+
+  resources :questions
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
