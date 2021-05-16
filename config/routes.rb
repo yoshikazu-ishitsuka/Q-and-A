15636ae3fb3deb355a26c0c43a85f1f27d16f44e
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-05-16 15:34:43 +0900

    rootメソッドの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # get 'questions/new'
   # get 'questions/edit'
 
+  root 'questions#index'
   resources :questions
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
