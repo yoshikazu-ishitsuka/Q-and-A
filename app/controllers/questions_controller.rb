50cef79b635c04cbfe9e00e8ec46bd246ae1e24d
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-05-21 10:26:24 +0900

    質問削除機能の追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -34,6 +34,12 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to root_path, notice: 'Success!'
+  end
+
   private
 
   def question_params
