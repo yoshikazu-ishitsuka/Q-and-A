e12b285c516d43c85c21737a7d15d777d91fefe7
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-05-23 09:35:07 +0900

    回答機能関連のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # get 'questions/edit'
 
   root 'questions#index'
-  resources :questions
+  resources :questions do
+    resources :answers
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
