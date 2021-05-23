f48d4940f7f228aa27952a6c69f2ab620bd5ecf7
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-05-23 09:33:49 +0900

    Answerモデルの作成

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
+  has_many :answers, dependent: :destroy
+
   validates :name, presence: true
   validates :title, presence: true
   validates :content, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_015848) do
+ActiveRecord::Schema.define(version: 2021_05_22_113106) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer "question_id"
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "name"
