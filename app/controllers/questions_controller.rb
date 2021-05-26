7fdc0e0ab8ebb0d542837554422d18719f757944
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-05-26 20:57:50 +0900

    QuestionsControllerのリファクタリング

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,12 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find(params[:id])
+    # @question = Question.find(params[:id])
     @answer = Answer.new
   end
 
@@ -23,11 +25,11 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find(params[:id])
+    # @question = Question.find(params[:id])
   end
 
   def update
-    @question = Question.find(params[:id])
+    # @question = Question.find(params[:id])
     if @question.update(question_params)
       redirect_to root_path, notice: 'Success!'
     else
@@ -37,7 +39,7 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question = Question.find(params[:id])
+    # @question = Question.find(params[:id])
     @question.destroy
     redirect_to root_path, notice: 'Success!'
   end
@@ -47,5 +49,9 @@ class QuestionsController < ApplicationController
   def question_params
     # byebug
     params.require(:question).permit(:name, :title, :content)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
